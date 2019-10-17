5961ce7277755be5eb255bb536ec1f93d10a9f21
Sunil Joshi <sunil.joshi@xyleminc.com>
2019-10-17 15:37:40 -0400

    Added supported languages API

--- src/main/java/com/arcsq/poc/springcloud/xi18n/helper/ResourceBundleConfigClient.java
+++ src/main/java/com/arcsq/poc/springcloud/xi18n/helper/ResourceBundleConfigClient.java
@@ -13,6 +13,10 @@ public interface ResourceBundleConfigClient {
     String getLanguagePack(@PathVariable("realm") String realm, @PathVariable("resource") String resource,
                            @PathVariable("language") String langId);
 
+    @GetMapping(path = "/config/{realm}/supported-languages.json")
+    String getSupportedLanguages(@PathVariable("realm") String realm, @PathVariable("resource") String resource,
+                           @PathVariable("language") String langId);
+
     @GetMapping(path = "/config/{realm}/xi18n-metadata.json")
     String getMetadata(@PathVariable("realm") String realm);
 
